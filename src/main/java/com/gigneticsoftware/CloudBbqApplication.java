351db20975627a08a82c36b9aea65acc387a8f5b
Akash_Darshan_Sagar <admin@gigneticsoftware.com>
2025-12-06 17:08:14 +0530

    Initial project commit

---
+++ src/main/java/com/gigneticsoftware/CloudBbqApplication.java
@@ -0,0 +1,13 @@
+package com.gigneticsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBbqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBbqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gigneticsoftware/CloudBbqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigneticsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBbqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
